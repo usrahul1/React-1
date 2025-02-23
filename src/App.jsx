fc3e8fc11c6f61cead4715194fa111363dac60a5
Sai Rahul <sairahulurumu@gmail.com>
2025-02-23 15:14:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
